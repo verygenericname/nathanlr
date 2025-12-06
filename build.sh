14b3b21f479b1d8ee9f6572da46d04afbd9670ae
verygenericname <dedbeddedbed@gmail.com>
2025-12-06 14:19:53 -0500

    don't need to do this anymore

--- build.sh
+++ build.sh
@@ -10,11 +10,9 @@ rm -rf Payload
 rm -rf nathanlr.tipa
 mkdir Payload
 mv NathanLR.app Payload
-ldid -S../../../../../usprebooter/usprebooter.entitlements Payload/NathanLR.app/NathanLR -Icom.nathan.nathanlr
-../../../../../macbins/ct_bypass -i Payload/NathanLR.app/NathanLR -o Payload/NathanLR.app/NathanLR -r
+codesign -f -s - Payload/NathanLR.app/NathanLR --entitlements ../../../../../usprebooter/usprebooter.entitlements --identifier com.nathan.nathanlr
 cp ../../../../../bins/* Payload/NathanLR.app/
 zip -vr nathanlr.tipa Payload/ -x "*.DS_Store"
 rm -rf Payload
 cd ../../../../../
-# scp -i/Users/nathan/Downloads/ssh-key-2024-05-25.key build/DerivedData/Build/Products/Release-iphoneos/nathanlr.tipa root@nathan4s.lol:/var/www/nathan4s.lol/html/nathanlr/nathanlr.tipa
 open build/DerivedData/Build/Products/Release-iphoneos
